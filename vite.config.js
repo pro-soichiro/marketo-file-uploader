3afddf4cec23ba72194eabf98046e90e7cf4b535
pro-soichiro <dr.1987626@gmail.com>
2023-08-07 19:08:02 +0900

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/marketo-file-uploader/',
+  // base: '/dist/',
+  // base: process.env.NODE_ENV === 'production' ? '/marketo-file-uploader/' : '/dist/',
   plugins: [
     vue(),
   ],
