5aa206e1e230e232ea320a10791a79d92ff0dc89
pro-soichiro <dr.1987626@gmail.com>
2023-08-21 12:17:11 +0900

    fix validation file

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,10 +1,10 @@
-const MAX_FILE_COUNT = 3 // 最大添付ファイル数
-const MAX_FILE_SIZE_MB = 3 // ファイルサイズの最大値 (MB)
-const ALLOWED_EXTENSIONS = ['pdf', 'docs', 'doc', 'xlsx', 'xls']
+export const MAX_FILE_COUNT = 2 // 最大添付ファイル数
+export const MAX_FILE_SIZE_MB = 3 // ファイルサイズの最大値 (MB)
+export const ALLOWED_EXTENSIONS = ['pdf', 'docs', 'doc', 'xlsx', 'xls']
 
 // ファイル数のチェック
 export function isValidFileCount(curCount, prevCount) {
-  return curCount + prevCount > MAX_FILE_COUNT
+  return curCount + prevCount <= MAX_FILE_COUNT
 }
 
 // ファイルの拡張子
