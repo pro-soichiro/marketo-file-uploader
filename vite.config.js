e802b94b7f080fd74cfb34087901a883cdfe20ee
pro-soichiro <dr.1987626@gmail.com>
2023-08-21 15:32:05 +0900

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -6,25 +6,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/marketo-file-uploader/',
-  // base: '/dist/',
-  // base: process.env.NODE_ENV === 'production' ? '/marketo-file-uploader/' : '/dist/',
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   build: {
-    // outDir: './path/to/', // ビルド成果物の生成先
-    manifest: true,
-    // rollupOptions: {
-    //   output: { // entry chunk assets それぞれの書き出し名の指定
-    //     entryFileNames: `assets/[name]-marketo-file-uploader.js`,
-    //     chunkFileNames: `assets/[name]-marketo-file-uploader.js`,
-    //     assetFileNames: `assets/[name]-marketo-file-uploader.[ext]`,
-    //   },
-    // },
-  },
+    manifest: true
+  }
 })
