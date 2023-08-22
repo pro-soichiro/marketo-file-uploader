582607b26fdf35f06798f673efbd42e47c7975c2
pro-soichiro <dr.1987626@gmail.com>
2023-08-22 12:16:04 +0900

    delete log script

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,17 +1,3 @@
-<script>
-// import imageSrc from '@/assets/img/logo.svg';
-// console.log(imageSrc)
-// fetch('https://pro-soichiro.github.io/marketo-file-uploader/manifest.json')
-//   .then((response) => response.json())
-//   .then((manifest) => {
-//     let img = document.createElement('img')
-//     img.alt = 'furien'
-//     img.style.height = '2.4rem'
-//     img.src = `https://pro-soichiro.github.io/marketo-file-uploader/${manifest['src/assets/img/logo.svg']['file']}`
-//     document.getElementById('headerLogo').appendChild(img)
-//   })
-</script>
-
 <template>
   <header class="header">
     <div id="headerLogo" class="header__logo">
