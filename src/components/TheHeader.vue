fcb502c2239f8b5ea78db1a3522b2966dddb979f
pro-soichiro <dr.1987626@gmail.com>
2023-08-22 12:04:17 +0900

    logoをpublicへ移動

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,19 +1,22 @@
 <script>
-fetch('https://pro-soichiro.github.io/marketo-file-uploader/manifest.json')
-  .then((response) => response.json())
-  .then((manifest) => {
-    let img = document.createElement('img')
-    img.alt = 'furien'
-    img.style.height = '2.4rem'
-    img.src = `https://pro-soichiro.github.io/marketo-file-uploader/${manifest['src/assets/img/logo.svg']['file']}`
-    document.getElementById('headerLogo').appendChild(img)
-  })
+// import imageSrc from '@/assets/img/logo.svg';
+// console.log(imageSrc)
+// fetch('https://pro-soichiro.github.io/marketo-file-uploader/manifest.json')
+//   .then((response) => response.json())
+//   .then((manifest) => {
+//     let img = document.createElement('img')
+//     img.alt = 'furien'
+//     img.style.height = '2.4rem'
+//     img.src = `https://pro-soichiro.github.io/marketo-file-uploader/${manifest['src/assets/img/logo.svg']['file']}`
+//     document.getElementById('headerLogo').appendChild(img)
+//   })
 </script>
 
 <template>
   <header class="header">
-    <div id="headerLogo" class="header__logo"></div>
-    <img src="@/assets/img/logo.svg" alt="" style="display: none;">
+    <div id="headerLogo" class="header__logo">
+      <img src="logo.svg" alt="furien" />
+    </div>
   </header>
 </template>
 
@@ -23,6 +26,9 @@ fetch('https://pro-soichiro.github.io/marketo-file-uploader/manifest.json')
 
   &__logo {
     text-align: center;
+    img {
+      height: 2.4rem;
+    }
   }
 }
 </style>
